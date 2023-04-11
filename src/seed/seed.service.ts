5a03a3935f94d9ddb3ec5c3e21246a8fd4a3d16d
sebasGZA <sebassga34@gmail.com>
2023-04-11 10:20:39 -0500

    Http request using axios

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  executeSeed() {
-    return 'Seeds executed';
+  private readonly axios: AxiosInstance = axios;
+
+  async executeSeed() {
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
+    );
+
+    const newData = data.results.map(({ name, url }) => {
+      const segments = url.split('/');
+      const no: number = +segments[segments.length - 2];
+
+      return { name, no };
+    });
+    return newData;
   }
 }
