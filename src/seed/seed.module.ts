2a6b682343dab91c1e96b95548d2733233c0899c
sebasGZA <sebassga34@gmail.com>
2023-04-11 10:53:49 -0500

    Create custom provider with axios

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+
+import { CommonModule } from 'src/common/common.module';
+import { PokemonModule } from 'src/pokemon/pokemon.module';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
-import { PokemonModule } from 'src/pokemon/pokemon.module';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
-  imports: [PokemonModule],
+  imports: [PokemonModule, CommonModule],
 })
 export class SeedModule {}
