f10c7d877eb2ad4710fcc305f7ac8da82b366a1b
sebasGZA <sebassga34@gmail.com>
2023-04-11 11:52:10 -0500

    ValidationSchema with Joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,13 @@ import { CommonModule } from './common/common.module';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { SeedModule } from './seed/seed.module';
 import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
